c53253a5385fe6f2579a5bd8352c615f736c615b
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2022-11-23 05:58:44 -0300

    Initial Commit

---
+++ PrimeiroAppTableViewCurso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppTableViewCurso
+//
+//  Created by Rodrigo Cerqueira Reis on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
